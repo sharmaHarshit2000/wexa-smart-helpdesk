04634b00e1fbe0579720dc15614fe4eaf1c1aa8e
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-08-27 18:10:14 +0530

    add jest

---
+++ backend/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.js'],
+  collectCoverageFrom: [
+    'routes/**/*.js',
+    'models/**/*.js',
+    'services/**/*.js',
+    'middleware/**/*.js',
+    '!**/node_modules/**'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  },
+  setupFiles: ['<rootDir>/jest.env.js'],
+};
